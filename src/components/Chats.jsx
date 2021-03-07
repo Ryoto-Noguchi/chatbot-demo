6e9337dae7ae6174572225766f0c7429dd6c6d04
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-03-08 07:22:50 +0900

    【実践編】#7 完了

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,21 +3,18 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import {Chat} from "./index";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: "36ch",
-    backgroundColor: theme.palette.background.paper,
-  },
-  inline: {
-    display: "inline",
-  },
-}));
+const useStyles = makeStyles({
+  chats: {
+    height: 400,
+    padding: '0',
+    overflow: 'auto' // スクロールできるようにする
+  }
+})
 
 const Chats = (props) => {
   const classes = useStyles();
   return (
-  <List className={classes.root}>
+  <List className={classes.chats}>
     {props.chats.map((chat, index) => {
       return <Chat text={chat.text} type={chat.type} key={index.toString()}/> // リスト要素の中にkeyを入れておくことで変更があったときに、変更部分のみ書き換えるという設計思想からkeyをリストに含めている
     })}
